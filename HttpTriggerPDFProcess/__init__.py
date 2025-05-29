d20f6eafb4e1cebf521947b41c4f696ccab86d7f
oufkirbrahim <brahim.oufkir@ted-company.com>
2025-05-29 02:25:58 +0200

    Update __init__.py

--- HttpTriggerPDFProcess/__init__.py
+++ HttpTriggerPDFProcess/__init__.py
@@ -1,11 +1,10 @@
 import azure.functions as func
 import sys
-import pkg_resources
+from functions import test_function
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     # Lister les packages installés
-    installed_packages = [d.project_name + "==" + d.version 
-                         for d in pkg_resources.working_set]
+    installed_packages = test_function()
     
     return func.HttpResponse(
         f"Packages installés:\n" + "\n".join(sorted(installed_packages)),
